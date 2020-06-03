9d987a3f52369211ae498b386a0d6d7d5e82d1db
nickclark2016 <nicholaus.clark115@gmail.com>
2020-06-03 18:58:11 -0400

    Update premake script to check for python3

--- install_glad.sh
+++ install_glad.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-pip install --upgrade git+https://github.com/dav1dde/glad.git@glad2#egg=glad
+pypip=pip
+command -v pip3 >/dev/null 2>&1 && pypip=pip3
+exec $pypip install --upgrade git+https://github.com/dav1dde/glad.git@glad2#egg=glad
--- premake_common.sh
+++ premake_common.sh
@@ -11,7 +11,9 @@ rm -rf bin-int/
 
 # Download GLAD Vulkan Headers
 rm -rf ./dependencies/glad/include
-python -m glad --out-path=./dependencies/glad --api=vulkan --extensions="VK_KHR_surface,VK_KHR_swapchain,\
+py=python
+command -v python3 >/dev/null 2>&1 && py=python3
+exec $py -m glad --out-path=./dependencies/glad --api=vulkan --extensions="VK_KHR_surface,VK_KHR_swapchain,\
     VK_EXT_debug_utils,VK_KHR_get_memory_requirements2,VK_KHR_get_physical_device_properties2,\
     VK_KHR_bind_memory2,VK_EXT_debug_report,VK_KHR_dedicated_allocation,VK_KHR_device_group,VK_KHR_buffer_device_address,\
     VK_EXT_buffer_device_address" c --loader --header
